a9dc39efba5b042ec8d7c7112105bd2238ab3328
sree arsha <sreearsha@srees-MacBook-Air.local>
2024-08-22 15:30:30 +0530

    Your commit message

--- MyServer.py
+++ MyServer.py
@@ -4,7 +4,7 @@ import threading
 
 HEADER = 64
 PORT = 5050
-SERVER = “138.68.140.83”
+SERVER = "138.68.140.83"
 print(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
@@ -15,6 +15,7 @@ server.bind(ADDR)
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
+    print("Poornasree")
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -24,7 +25,7 @@ def handle_client(conn, addr):
             if msg == DISCONNECT_MESSAGE:
                 connected = False
             print(f"[{addr}] {msg}")
-        conn.send(f“Thanks from server for your message: ({msg})“.encode(FORMAT))
+        conn.send(f"Thanks from server for your message: ({msg})".encode(FORMAT))
 
     conn.close()
 
